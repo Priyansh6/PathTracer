cb28911152845e0947be4115ce54ca10120f272a
Priyansh Mahajan <priyanshmahajan@gmail.com>
2025-08-16 16:27:11 +0100

    Partition spheres in bvh at optimal point using heuristic

--- src/data_structures/SphereBVH.cpp
+++ src/data_structures/SphereBVH.cpp
@@ -7,16 +7,21 @@
 #include <algorithm>
 #include <cstdint>
 #include <limits>
+#include <print>
 #include <ranges>
 #include <span>
 #include <utility>
 #include <vector>
 
+using namespace sphere_bvh;
+
 SphereBVH::SphereBVH(const std::span<const Sphere> spheres)
   : m_spheres{ spheres }, m_sphere_indices(std::views::iota(0U, spheres.size()) | std::ranges::to<std::vector>())
 {
   m_nodes.reserve((2 * m_spheres.size()) - 1);
+  std::println("{}", m_nodes.size());
   build_bvh();
+  std::println("{}", m_nodes.size());
 }
 
 thread_local std::vector<std::uint32_t> SphereBVH::m_traversal_stack;
@@ -24,7 +29,7 @@ thread_local std::vector<std::uint32_t> SphereBVH::m_traversal_stack;
 bool SphereBVH::intersect(const Ray& r, const double t_min, const double t_max, HitRecord& rec) const
 {
   auto closest_t = t_max;
-  const Vec3 r_dir_inv = 1.0 / r.direction();
+  const Vec3 r_dir_reciprocal = 1.0 / r.direction();
 
   m_traversal_stack.clear();
   m_traversal_stack.reserve(m_nodes.size());
@@ -35,7 +40,7 @@ bool SphereBVH::intersect(const Ray& r, const double t_min, const double t_max, 
     m_traversal_stack.pop_back();
     const SphereBVHNode& node = m_nodes[node_i];
 
-    if (!intersects_aabb(node, r, r_dir_inv, t_min, t_max)) { continue; }
+    if (!intersects_aabb(node, r, r_dir_reciprocal, t_min, t_max)) { continue; }
     if (node.is_leaf()) {
       for (std::uint32_t i = node.left_first; i < (node.left_first + node.sphere_count); i++) {
         HitRecord temp_rec{};
@@ -56,20 +61,26 @@ bool SphereBVH::intersect(const Ray& r, const double t_min, const double t_max, 
 void SphereBVH::build_bvh()
 {
   constexpr std::uint32_t root_node_i = 0U;
-  auto [aabb_min, aabb_max] = get_node_bounds(root_node_i, m_spheres.size());
-  m_nodes.emplace_back(aabb_min, aabb_max, 0U, m_spheres.size());
+  constexpr std::uint32_t first_sphere_i = 0U;
+  const AABB aabb = get_node_bounds(first_sphere_i, m_spheres.size());
+  m_nodes.emplace_back(aabb, first_sphere_i, m_spheres.size());
   subdivide(m_nodes[root_node_i]);
 }
 
 void SphereBVH::subdivide(SphereBVHNode& node)
 {
-  if (node.sphere_count <= 1) { return; }
+  Vec3 extent = node.aabb.bmax - node.aabb.bmin;
+  const double half_parent_surface_area =
+    (extent.x() * extent.y()) + (extent.x() * extent.z()) + (extent.y() * extent.z());
+  const double parent_cost = half_parent_surface_area * node.sphere_count;
 
-  auto [axis, splitPos] = find_best_split(node);
+  if (node.sphere_count <= 1) { return; }
+  auto [axis, split_pos, cost] = find_best_split(node);
+  if (cost >= parent_cost) { return; }
 
   const std::uint32_t l = node.left_first;
   const std::uint32_t r = node.left_first + node.sphere_count - 1;
-  const std::uint32_t split_i = partition_spheres(l, r, axis, splitPos);
+  const std::uint32_t split_i = partition_spheres(l, r, axis, split_pos);
 
   const std::uint32_t left_count = split_i - l;
   if (left_count == 0 || left_count == node.sphere_count) { return; }
@@ -81,11 +92,11 @@ void SphereBVH::subdivide(SphereBVHNode& node)
   const std::uint32_t right_left_first = split_i;
   const std::uint32_t right_sphere_count = node.sphere_count - left_count;
 
-  auto [left_aabb_min, left_aabb_max] = get_node_bounds(left_left_first, left_sphere_count);
-  auto [right_aabb_min, right_aabb_max] = get_node_bounds(right_left_first, right_sphere_count);
+  const AABB left_aabb = get_node_bounds(left_left_first, left_sphere_count);
+  const AABB right_aabb = get_node_bounds(right_left_first, right_sphere_count);
 
-  m_nodes.emplace_back(left_aabb_min, left_aabb_max, left_left_first, left_sphere_count);
-  m_nodes.emplace_back(right_aabb_min, right_aabb_max, right_left_first, right_sphere_count);
+  m_nodes.emplace_back(left_aabb, left_left_first, left_sphere_count);
+  m_nodes.emplace_back(right_aabb, right_left_first, right_sphere_count);
 
   node.sphere_count = 0U;
   node.left_first = left_i;
@@ -94,23 +105,14 @@ void SphereBVH::subdivide(SphereBVHNode& node)
   subdivide(m_nodes[left_i + 1]);
 }
 
-std::pair<Point3, Point3> SphereBVH::get_node_bounds(const std::uint32_t first_sphere_i,
-  const std::uint32_t sphere_count) const
+AABB SphereBVH::get_node_bounds(const std::uint32_t first_sphere_i, const std::uint32_t sphere_count) const
 {
-  std::pair<Point3, Point3> bounds;
-  auto& [aabb_min, aabb_max] = bounds;
-  aabb_min = Point3{ std::numeric_limits<double>::max() };
-  aabb_max = Point3{ std::numeric_limits<double>::lowest() };
+  AABB aabb{};
   for (std::uint32_t i = first_sphere_i; i < (first_sphere_i + sphere_count); i++) {
     const Sphere& sphere = m_spheres[m_sphere_indices[i]];
-    aabb_min.x() = std::min(aabb_min.x(), sphere.centre().x() - sphere.radius());
-    aabb_min.y() = std::min(aabb_min.y(), sphere.centre().y() - sphere.radius());
-    aabb_min.z() = std::min(aabb_min.z(), sphere.centre().z() - sphere.radius());
-    aabb_max.x() = std::max(aabb_max.x(), sphere.centre().x() + sphere.radius());
-    aabb_max.y() = std::max(aabb_max.y(), sphere.centre().y() + sphere.radius());
-    aabb_max.z() = std::max(aabb_max.z(), sphere.centre().z() + sphere.radius());
+    aabb.grow_to_fit_sphere(sphere);
   }
-  return bounds;
+  return aabb;
 }
 
 std::uint32_t SphereBVH::partition_spheres(std::uint32_t l, std::uint32_t r, const int axis, const double split_val)
@@ -129,35 +131,61 @@ std::uint32_t SphereBVH::partition_spheres(std::uint32_t l, std::uint32_t r, con
 
 bool SphereBVH::intersects_aabb(const SphereBVHNode& node,
   const Ray& r,
-  const Vec3& r_dir_inv,
+  const Vec3& r_dir_reciprocal,
   double t_min,
   double t_max)
 {
-  const double tx1 = (node.aabb_min.x() - r.origin().x()) * r_dir_inv.x();
-  const double tx2 = (node.aabb_max.x() - r.origin().x()) * r_dir_inv.x();
+  const double tx1 = (node.aabb.bmin.x() - r.origin().x()) * r_dir_reciprocal.x();
+  const double tx2 = (node.aabb.bmax.x() - r.origin().x()) * r_dir_reciprocal.x();
 
   t_min = std::max(t_min, std::min(tx1, tx2));
   t_max = std::min(t_max, std::max(tx1, tx2));
 
-  const double ty1 = (node.aabb_min.y() - r.origin().y()) * r_dir_inv.y();
-  const double ty2 = (node.aabb_max.y() - r.origin().y()) * r_dir_inv.y();
+  const double ty1 = (node.aabb.bmin.y() - r.origin().y()) * r_dir_reciprocal.y();
+  const double ty2 = (node.aabb.bmax.y() - r.origin().y()) * r_dir_reciprocal.y();
   t_min = std::max(t_min, std::min(ty1, ty2));
   t_max = std::min(t_max, std::max(ty1, ty2));
 
-  const double tz1 = (node.aabb_min.z() - r.origin().z()) * r_dir_inv.z();
-  const double tz2 = (node.aabb_max.z() - r.origin().z()) * r_dir_inv.z();
+  const double tz1 = (node.aabb.bmin.z() - r.origin().z()) * r_dir_reciprocal.z();
+  const double tz2 = (node.aabb.bmax.z() - r.origin().z()) * r_dir_reciprocal.z();
   t_min = std::max(t_min, std::min(tz1, tz2));
   t_max = std::min(t_max, std::max(tz1, tz2));
 
   return t_max >= t_min;
 }
 
-std::pair<int, double> SphereBVH::find_best_split(const SphereBVHNode& node)
+SplitResult SphereBVH::find_best_split(const SphereBVHNode& node) const
 {
-  Vec3 extent = node.aabb_max - node.aabb_min;
-  int axis = 0;
-  if (extent.y() > extent.x()) { axis = 1; }
-  if (extent.z() > extent[axis]) { axis = 2; }
-  const double splitPos = (node.aabb_min[axis] + node.aabb_max[axis]) / 2.0;
-  return { axis, splitPos };
+  SplitResult best_split{ 0, 0.0, std::numeric_limits<double>::max() };
+  for (int axis = 0; axis < 3; axis++) {
+    for (uint32_t i = 0U; i < node.sphere_count; i++) {
+      const Sphere& sphere = m_spheres[m_sphere_indices[node.left_first + i]];
+      const double split_pos = sphere.centre()[axis];
+      if (const double cost = evaluate_surface_area_heuristic(node, axis, split_pos); cost < best_split.cost) {
+        best_split.cost = cost;
+        best_split.axis = axis;
+        best_split.split_value = split_pos;
+      }
+    }
+  }
+  return best_split;
+}
+
+double
+  SphereBVH::evaluate_surface_area_heuristic(const SphereBVHNode& node, const int axis, const double split_pos) const
+{
+  AABB left_box{};
+  AABB right_box{};
+  int left_count = 0;
+  int right_count = 0;
+  for (std::uint32_t i = 0U; i < node.sphere_count; i++) {
+    if (const Sphere& sphere = m_spheres[m_sphere_indices[node.left_first + i]]; sphere.centre()[axis] < split_pos) {
+      left_count++;
+      left_box.grow_to_fit_sphere(sphere);
+    } else {
+      right_count++;
+      right_box.grow_to_fit_sphere(sphere);
+    }
+  }
+  return (left_count * left_box.half_surface_area()) + (right_count * right_box.half_surface_area());
 }
--- src/data_structures/SphereBVH.h
+++ src/data_structures/SphereBVH.h
@@ -6,20 +6,55 @@
 #include "entities/Sphere.h"
 #include "entities/Vec3.h"
 
-#include <cstddef>
+#include <algorithm>
 #include <cstdint>
+#include <limits>
 #include <span>
 #include <utility>
 #include <vector>
 
+namespace sphere_bvh {
+struct AABB
+{
+  Point3 bmin{ std::numeric_limits<double>::max() };
+  Point3 bmax{ std::numeric_limits<double>::lowest() };
+
+  void grow_to_fit_sphere(const Sphere& sphere)
+  {
+    bmin.x() = std::min(bmin.x(), sphere.centre().x() - sphere.radius());
+    bmin.y() = std::min(bmin.y(), sphere.centre().y() - sphere.radius());
+    bmin.z() = std::min(bmin.z(), sphere.centre().z() - sphere.radius());
+    bmax.x() = std::max(bmax.x(), sphere.centre().x() + sphere.radius());
+    bmax.y() = std::max(bmax.y(), sphere.centre().y() + sphere.radius());
+    bmax.z() = std::max(bmax.z(), sphere.centre().z() + sphere.radius());
+  }
+
+  [[nodiscard]] double half_surface_area() const
+  {
+    const Vec3 extent = bmax - bmin;
+    return (extent.x() * extent.y()) + (extent.x() * extent.z()) + (extent.y() * extent.z());
+  }
+};
+
+struct SplitResult
+{
+  int axis;
+  double split_value;// Anything lower non-inclusive should be on the left
+  double cost;
+
+  SplitResult(const int axis, const double split_value, const double cost)
+    : axis{ axis }, split_value{ split_value }, cost{ cost } {};
+};
+
 struct SphereBVHNode
 {
-  Point3 aabb_min, aabb_max;
+  sphere_bvh::AABB aabb;
   std::uint32_t left_first{ 0U };// Index of either the first child node or the first sphere in this node
   std::uint32_t sphere_count{ 0U };// Number of speres in this node, or 0 if this is not a leaf node
 
   [[nodiscard]] bool is_leaf() const { return sphere_count > 0; }
 };
+}// namespace sphere_bvh
 
 class SphereBVH
 {
@@ -29,22 +64,24 @@ public:
 
 private:
   void build_bvh();
-  void subdivide(SphereBVHNode& node);
-  // Gets the aabb_min and aabb_max of all spheres in the range [first_sphere_i, first_sphere_i + sphere_count)
-  [[nodiscard]] std::pair<Point3, Point3> get_node_bounds(std::uint32_t first_sphere_i,
-    std::uint32_t sphere_count) const;
+  void subdivide(sphere_bvh::SphereBVHNode& node);
+  // Gets the bmin and bmax of all spheres in the range [first_sphere_i, first_sphere_i + sphere_count)
+  [[nodiscard]] sphere_bvh::AABB get_node_bounds(std::uint32_t first_sphere_i, std::uint32_t sphere_count) const;
   // Partitions the spheres in the range [l, r] (inclusive) based on the split value along the given axis
   // and returns the index of the first sphere in the right partition.
   std::uint32_t partition_spheres(std::uint32_t l, std::uint32_t r, int axis, double split_val);
-  [[nodiscard]] static bool
-    intersects_aabb(const SphereBVHNode& node, const Ray& r, const Vec3& r_dir_inv, double t_min, double t_max);
-
-  // Finds the best split for the given node and returns the axis and split value.
-  [[nodiscard]] static std::pair<int, double> find_best_split(const SphereBVHNode& node);
+  [[nodiscard]] static bool intersects_aabb(const sphere_bvh::SphereBVHNode& node,
+    const Ray& r,
+    const Vec3& r_dir_reciprocal,
+    double t_min,
+    double t_max);
+  [[nodiscard]] sphere_bvh::SplitResult find_best_split(const sphere_bvh::SphereBVHNode& node) const;
+  [[nodiscard]] double
+    evaluate_surface_area_heuristic(const sphere_bvh::SphereBVHNode& node, int axis, double split_pos) const;
 
   std::span<const Sphere> m_spheres;
   std::vector<std::uint32_t> m_sphere_indices;
-  std::vector<SphereBVHNode> m_nodes;
+  std::vector<sphere_bvh::SphereBVHNode> m_nodes;
   thread_local static std::vector<std::uint32_t> m_traversal_stack;
 };
 
